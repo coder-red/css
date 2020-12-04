fc7a9c87ffe72f1581ec20aaa9e8aec2e2f80384
Ahmed <babatundeisyoruba@gmail.com>
2020-12-04 09:39:13 +0100

    files upload

---
+++ git.js
@@ -0,0 +1,25 @@
+//Problem: Hints are shown even when form is valid
+//Solution: Hide and show them at appropriate times
+alert('hi');
+console.log('jkj');
+
+//Hide hints
+ $("form span").hide();
+    //Find out if password is valid
+
+      //Hide hint if valid
+
+      //else show hint
+
+
+
+  //Find out if password and confirmation match
+
+    //Hide hint if match
+
+    //else show hint
+
+
+//When event happens on password input
+
+//When event happens on confirmation input
